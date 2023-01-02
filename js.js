3ec6dbb0c1bf5bd2eb415c3e1c6fb8cbe0ace0e3
Yasar Curci <yasarcu@hotmail.com>
2023-01-02 22:12:40 +0300

    Update js.js

--- js.js
+++ js.js
@@ -25,7 +25,7 @@
 
 	function newQuestion() {
 		num1 = Math.floor(Math.random() * 11); // generate new random number from 0 to 10
-		num2 = Math.floor(Math.random() * (20 - num1 + 1)) + num1; // generate new random number from num1 to 20
+		num2 = Math.floor(Math.random() * (10 - num1 + 1)) + num1; // generate new random number from num1 to 20
 		document.getElementById("question").innerHTML = num1 + " + " + num2 + " = ";
 		document.getElementById("answer").value = "";
 		document.body.style.backgroundColor = "yellow";
